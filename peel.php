1b67bbb935d8a75105287db0692994861107b643
rmkn <bubblun@gmail.com>
2014-07-29 18:32:35 +0900

    Update peel.php

--- peel.php
+++ peel.php
@@ -311,51 +311,49 @@ class D
 }
 
 /***
+フレームワーク的なもの
+======================
 
 使い方
-======
+------
 
-フロントコントローラー
-----------------------
+### フロントコントローラー
 
-### 生成
+#### 生成
 - 出力フォーマットを指定する(Peel::FORMAT_HTML | Peel::FORMAT_JSON)
--- アプリの場合
+  - アプリの場合
     > $fcon = new PeelController();
--- APIの場合
+
+  - APIの場合
     > $fcon = new PeelController(Peel::FORMAT_JSON);
 
-### 実行
-    > $fcon->execute();
+####実行
+  > $fcon->execute();
 
-コントローラー
---------------
+### コントローラー
 
-### 設置場所
+#### 設置場所
 - controllerディレクトリ
 
-### 実装
+#### 実装
 - Peelクラスを継承
 - 次のいずれかが必要(GET /controller/method/index の場合)
-    1. getIndex()
-    2. execIndex()
-    3. execGet()
+  1. getIndex()
+  2. execIndex()
+  3. execGet()
 - prepare()メソッドがあれば最初に呼び出される
 - finish()メソッドがあれば最後に呼び出される
 
-ビュー
-------
+### ビュー
 
-### 設置場所
+#### 設置場所
 - templateディレクトリ(にinclude_pathが設定されているだけ)
 
-### 実装
+#### 実装
 - 素のPHPで作ってコントローラーからincludeとか
 
-ライブラリ
-----------
+### ライブラリ
 
-### 設置場所
+#### 設置場所
 - libディレクトリ(にinclude_pathが設定されているだけ)
-
 */

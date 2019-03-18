8519f9bf58817fe84bf1877e236f9742405592d1
rmkn <bubblun@gmail.com>
2019-03-18 11:06:26 +0900

    add sample-code

--- install.sh
+++ install.sh
@@ -3,3 +3,36 @@
 mkdir ./lib ./action ./template
 curl -o ./lib/peel.php https://raw.githubusercontent.com/rmkn/peel/master/peel.php
 curl -o .htaccess https://raw.githubusercontent.com/rmkn/peel/master/.htaccess
+
+
+cat << 'EOS' >> ./action/default.php
+<?php
+// vim: set et ts=4 sw=4 sts=4:
+
+class DefaultAction extends Peel
+{
+    public function prepare()
+    {
+        return true;
+    }
+
+    public function execGet()
+    {
+        echo __METHOD__;
+        return true;
+    }
+}
+EOS
+
+cat << 'EOS' >> ./index.php
+<?php
+// vim: set et ts=4 sw=4 sts=4 :
+
+require_once 'lib/peel.php';
+
+$fcon = new PeelController();
+$fcon->execute();
+
+//$fcon = new PeelController(Peel::FORMAT_JSON);
+//$fcon->execute(true);
+EOS
